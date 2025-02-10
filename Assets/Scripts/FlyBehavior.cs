88ead67aa6eac8ce9e23b52026b01c817bf001f1
Claire Liang <caliang@usc.edu>
2025-02-10 12:52:02 -0800

    Platform base

---
+++ Assets/Scripts/FlyBehavior.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class FlyBehavior : MonoBehaviour
+{
+    [SerializeField] private float _velocity = 1.5f;
+
+    private Rigidbody2D _rigidbody2D;
+
+    private void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            _rigidbody2D.linearVelocity = Vector2.up * _velocity;
+        }
+    }
+}
---
+++ Assets/Scripts/MoveLevel.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class MoveLevel : MonoBehaviour
+{
+    [SerializeField] private float _velocity = 0.65f;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Vector3.left * _velocity * Time.deltaTime;
+    }
+}
